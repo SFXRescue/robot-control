04af8fa00cdea0f18eac2024a09cee589af90929
Anthony Gambale <ants.gambale@gmail.com>
2021-07-25 16:30:00 +1000

    wrote new shell script that takes a command line argument to run the old or new program

---
+++ server/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+argument1=$1
+if [ $argument1 = -o ]
+then
+    echo Running the old sart junior code, from the 2021 competition.
+    python3 main.py &
+    python3 motor_control.py 
+    killall python3
+else
+    echo Running the sart-control server...
+    python3 app.py
+    killall python3
+fi

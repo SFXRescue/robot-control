16e550686c397c542807b4c13f60b20f17c817b0
Anthony Gambale <ants.gambale@gmail.com>
2021-07-25 16:45:13 +1000

    wrote it properly this time

--- server/run.sh
+++ server/run.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-argument1=$1
-if [ $argument1 = -o ]
+if [ "$1" = "-o" ];
 then
-    echo Running the old sart junior code, from the 2021 competition.
+    echo "Running the old sart junior code, from the 2021 competition."
     python3 main.py &
     python3 motor_control.py 
     killall python3
 else
-    echo Running the robot-control server...
+    echo "Running the robot-control server..."
     python3 app.py
     killall python3
 fi

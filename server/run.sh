1139f55d3e53dbc8be930e0ba92e905fed6fca9a
Anthony Gambale <ants.gambale@gmail.com>
2021-07-25 20:29:19 +1000

    fixed another typo

--- server/run.sh
+++ server/run.sh
@@ -2,7 +2,7 @@
 if [ "$1" = "-o" ];
 then
     echo "Running the old sart junior code, from the 2021 competition."
-    python3 main.py &
+    python3 app.py &
     python3 motor_control.py 
     killall python3
 else
